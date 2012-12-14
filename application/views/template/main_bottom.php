78d7404ad20dc0bc45c6b0458c48edc1d12883db
Sean McMahan (MaverickXS) <contact@seanmcmahan.info>
2012-12-14 15:52:23 -0500

    Fixed Alena's Derby name/number in footer

--- application/views/template/main_bottom.php
+++ application/views/template/main_bottom.php
@@ -1,12 +1,12 @@
 								</div>
-	                           
+
 	                        </div>
 	                    </div>
 	                </div>
 	            </div>
 	        </div>
 	    </section>
-	    
+
 		<!--==============================footer=================================-->
 	    <footer>
 	        <div class="main">
@@ -14,7 +14,7 @@
 	            	<div class="wrapper">
 	                	<div class="prefix_7 grid_17">
 	                    	&copy;Copyright 2012 <a href="/">Spartanburg Deadly Dolls</a>. All Rights Reserved. | <a href="https://www.facebook.com/spddolls" target="_blank">Facebook</a> | <a href="https://plus.google.com/b/115555959762600911552/115555959762600911552/posts" target="_blank">Google+</a><br/>
-	                    	<small class="footer_small">Site Developed by #TK421 Sean "Under-toe" McMahan<br/>Site Design by #.357 Alena "Bullet WithAName" McMahan</small>
+	                    	<small class="footer_small">Site Developed by #TK421 Sean "Under-toe" McMahan<br/>Site Design by #&lt;3 Alena "Snickers" McMahan</small>
 	                    	<div class="bl">
 	                    		<div class="fb-like" data-href="http://www.spddolls.com" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-colorscheme="dark" data-font="verdana"></div>
 	                    		<!-- Place this tag where you want the +1 button to render. -->
