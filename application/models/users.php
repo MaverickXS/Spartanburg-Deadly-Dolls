480276218f70a728cb2a517f1028ffe7fbff4a27
Sean McMahan (MaverickXS) <contact@seanmcmahan.info>
2013-02-11 10:36:44 -0500

    Updated meet the dolls with images.

--- application/models/users.php
+++ application/models/users.php
@@ -26,18 +26,19 @@ class Users extends CI_Model{
 		$this->db->order_by('Min(user_attributes.group_pos), user_attributes.`Group`, user_attributes.`pos`, user_attributes.`attribute`, user_attributes.`value`', '', false);
 		return $this->db->get();
 	}
-	
+
 	public function get_roster($team = 1, $active = 1){
 		$this->db->select('*, Replace(`users`.`skate_name`, \'The \', \'\') As Sort_Skate_Name', false);
 		$this->db->from('users');
 		$this->db->join('user_team_map', 'user_team_map.u_id = users.u_key');
+		$this->db->where('users.is_staff !=', 1);
 		$this->db->where('user_team_map.t_id', $team);
 		$this->db->where('user_team_map.is_active', $active);
 		$this->db->order_by('Sort_Skate_Name, users.number', '');
 
 		return $this->db->get();
 	}
-	
+
 	public function get_staff($team = 1, $active = 1){
 		$this->db->select('*, Replace(`users`.`skate_name`, \'The \', \'\') As Sort_Skate_Name', false);
 		$this->db->from('users');
@@ -63,7 +64,7 @@ class Users extends CI_Model{
 
 	public function update_user($u_key, $post_data){
 		$this->db->where('u_key', $u_key);
-		$this->db->update('users', $post_data); 
+		$this->db->update('users', $post_data);
 	}
 }
 ?>
--- application/views/meet_the_dolls_view.php
+++ application/views/meet_the_dolls_view.php
@@ -1,27 +1,39 @@
 <?
 if (isset($skaters)){
-	if ($skaters->num_rows() > 0){
-		?><h2>Skaters</h2><?
-		foreach ($skaters->result() as $user){
-			?>
-			<div class="wrapper">
-				<span class="number_wrap"><?=draw_number($user, true);?>&nbsp;</span> <a href="/view_player/<?=draw_uri_name($user);?>/"><?=draw_name($user);?></a>
-			</div>
-			<?
-		}
+	if ($skaters->num_rows() > 0){ ?>
+		<h2>Skaters</h2>
+		<ul class="thumbnails">
+			<? foreach ($skaters->result() as $user){
+				?>
+				<li class="span3">
+					<!--<a href="/view_player/<?=draw_uri_name($user);?>/" class="thumbnail">-->
+					<div class="thumbnail">
+						<img src="/img/users/<?=$user->u_key;?>.jpg" onerror="jQuery(this).attr('src', '/img/nopic.jpg');" />
+						<p style="margin-top: 10px;"><span class="number_wrap"><?=draw_number($user, true);?>&nbsp;</span> <strong style="color: #EE227B;"><?=draw_name($user);?></strong></p>
+					</div>
+				</li>
+				<?
+			}
+		?></ul><?
 	}
 }
 
 if (isset($staff)){
-	if ($staff->num_rows() > 0){
-		?><br/><h2>Staff</h2><?
-		foreach ($staff->result() as $user){
-			?>
-			<div class="wrapper">
-				<span class="number_wrap"><?=draw_number($user, true);?>&nbsp;</span> <a href="/view_player/<?=draw_uri_name($user);?>/"><?=draw_name($user);?></a>
-			</div>
-			<?
-		}
+	if ($staff->num_rows() > 0){ ?>
+		<br/><h2>Staff</h2>
+		<ul class="thumbnails">
+			<? foreach ($staff->result() as $user){
+				?>
+				<li class="span3">
+					<!--<a href="/view_player/<?=draw_uri_name($user);?>/" class="thumbnail">-->
+					<div class="thumbnail">
+						<img src="/img/users/<?=$user->u_key;?>.jpg" onerror="jQuery(this).attr('src', '/img/nopic.jpg');" />
+						<p style="margin-top: 10px;"><span class="number_wrap"><?=draw_number($user, true);?>&nbsp;</span> <strong style="color: #EE227B;"><?=draw_name($user);?></strong></p>
+					</div>
+				</li>
+				<?
+			}
+		?></ul><?
 	}
 }
 ?>
